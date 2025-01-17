6908dbd97aa9aaefa7a5a5bee6111525591ab390
Mateus Perfigao Domiciano <mateusperdigao@hotmail.com>
2025-01-17 19:51:08 +0000

    test

--- codeql-many-languages.sh
+++ codeql-many-languages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Certify that your runner has unzip 
-#sudo apt update
-#sudo apt install unzip
+sudo apt update
+sudo apt install unzip
 
 # Install github linguist to extract repository languages
 sudo apt-get install build-essential cmake pkg-config libicu-dev zlib1g-dev libcurl4-openssl-dev libssl-dev ruby-dev
@@ -11,10 +11,10 @@ sudo gem install github-linguist
 # Download CodeQL for Linux with cURL
 wget -q https://github.com/github/codeql-cli-binaries/releases/download/v2.20.1/codeql-linux64.zip
 mkdir $HOME/codeql-home
-unzip codeql-linux64.zip -d $HOME/codeql-home
+unzip -q codeql-linux64.zip -d $HOME/codeql-home
 
 # Download queries and add them to the CodeQL home folder
-#git clone --recursive https://github.com/github/codeql.git $HOME/codeql-home/codeql-repo
+git clone --recursive https://github.com/github/codeql.git $HOME/codeql-home/codeql-repo
 
 # Check the configuration
 $HOME/codeql-home/codeql/codeql resolve languages
@@ -22,9 +22,7 @@ $HOME/codeql-home/codeql/codeql resolve packs
 
 ls -l
 
-cd $HOME/codeql-home/codeql-repo
 github-linguist
-
 declare -A language_map=(
     ["C++"]="cpp"
     ["C#"]="csharp"
@@ -55,10 +53,7 @@ codeql_supported_languages=$(echo "${filtered_languages[*]}" | tr ' ' ',' | tr '
 echo "Languages detected by GitHub Linguist: $codeql_supported_languages"
 
 # Build and create CodeQL database
-$HOME/codeql-home/codeql/codeql database create codeqldb \ 
---db-cluster \
---language=$codeql_supported_languages \
---threads=4 
+$HOME/codeql-home/codeql/codeql database create codeqldb --db-cluster --language=$codeql_supported_languages --threads=4 
 
 # # Code Scanning suite: Queries run by default in CodeQL code scanning on GitHub.
 # # Default: python-code-scanning.qls
